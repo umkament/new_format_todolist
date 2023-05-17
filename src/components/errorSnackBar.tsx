089eb77a86dffac797933bb67ce3e76173900488
umkament <97944243+umkament@users.noreply.github.com>
2023-05-17 13:35:05 +0300

    добавлена универсальная компонента всплывающая ошибка

--- src/components/errorSnackBar.tsx
+++ src/components/errorSnackBar.tsx
@@ -1,2 +1,45 @@
-import React from 'react'
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import {useCommonDispatch, useCommonSelector} from "../store/store";
+import {setAppErrorAC} from "../state/app-reducer";
 
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+   props,
+   ref,
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export function ErrorSnackBar() {
+  const dispatch = useCommonDispatch()
+  const error = useCommonSelector(state => state.app.appError)
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    dispatch(setAppErrorAC(null))
+  };
+
+  const isOpen = error !== null
+
+  return (
+     <Stack spacing={2} sx={{ width: '100%' }}>
+       <Button variant="outlined" >
+         Open success snackbar
+       </Button>
+       <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
+         <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+           {error}
+         </Alert>
+       </Snackbar>
+       <Alert severity="error">This is an error message!</Alert>
+       <Alert severity="warning">This is a warning message!</Alert>
+       <Alert severity="info">This is an information message!</Alert>
+       <Alert severity="success">This is a success message!</Alert>
+     </Stack>
+  );
+}
