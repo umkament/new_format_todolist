ac1a372020dcd1f8460de55bd13979a22c752588
umkament <97944243+umkament@users.noreply.github.com>
2023-05-20 18:29:14 +0300

    .

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,43 +13,43 @@ import {useCommonDispatch, useCommonSelector} from "../store/store";
 import {alpha, styled} from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 
-type AppPropsType={
+type AppPropsType = {
   demo?: boolean
 }
 
 export const App: React.FC<AppPropsType> = (props) => {
-    const dispatch = useCommonDispatch()
-    const status = useCommonSelector(state => state.app.appStatus)
-    const initialized = useCommonSelector(state => state.app.initialized)
-    const isLoggedIn = useCommonSelector(state => state.auth.isLoggedIn)
+  const dispatch = useCommonDispatch()
+  const status = useCommonSelector(state => state.app.appStatus)
+  const initialized = useCommonSelector(state => state.app.initialized)
+  const isLoggedIn = useCommonSelector(state => state.auth.isLoggedIn)
 
-    useEffect(() => {
+  useEffect(() => {
     dispatch(initializedAppTC())
   }, [])
 
-    const logOutHandler = useCallback(() => {
+  const logOutHandler = useCallback(() => {
     dispatch(logOutTC())
   }, [dispatch])
 
-    if (!initialized) {
+  if (!initialized) {
     return <LinearProgress/>
   }
 
-    const Search = styled('div')(({theme}) => ({
+  const Search = styled('div')(({theme}) => ({
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
     '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
   }));
-    const SearchIconWrapper = styled('div')(({theme}) => ({
+  const SearchIconWrapper = styled('div')(({theme}) => ({
     padding: theme.spacing(0, 2),
     height: '100%',
     position: 'absolute',
@@ -58,24 +58,24 @@ export const App: React.FC<AppPropsType> = (props) => {
     alignItems: 'center',
     justifyContent: 'center',
   }));
-    const StyledInputBase = styled(InputBase)(({theme}) => ({
+  const StyledInputBase = styled(InputBase)(({theme}) => ({
     color: 'inherit',
     '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-    width: '12ch',
-    '&:focus': {
-    width: '20ch',
-  },
-  },
-  },
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
   }));
 
-    return <Box sx={{flexGrow: 1}}>
+  return <Box sx={{flexGrow: 1}}>
     <AppBar position="static">
       <Toolbar>
         <IconButton
